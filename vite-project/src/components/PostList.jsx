61a41b6eec8ca6dc713103d73a6173b4f0c27858
ibrahim <ibrahimskce@gmail.com>
2024-01-16 13:24:07 +0530

    done

--- vite-project/src/components/PostList.jsx
+++ vite-project/src/components/PostList.jsx
@@ -1,25 +1,16 @@
 import Post from "./Post";
 import classes from "./PostList.module.css";
-import NewPost from "./NewPost";
-import Modal from "./Modal";
-import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
-const PostList = ({ isPosting, onStopPosting }) => {
-  const [posts, setPosts] = useState([]);
-  function addPostHandler(postData) {
-    setPosts((existingPosts) => [postData, ...existingPosts]);
-  }
+const PostList = () => {
+  const posts = useLoaderData();
+  
   return (
     <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
-        </Modal>
-      )}
       {posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
-            <Post key={post.body} author={post.author} body={post.body} />
+            <Post key={post.id} id={post.id} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
@@ -29,6 +20,7 @@ const PostList = ({ isPosting, onStopPosting }) => {
           <p>Start adding some</p>
         </div>
       )}
+      
     </>
   );
 };
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Posts, { loader as postsLoader } from "./routes/Posts.jsx";
+import NewPost, { action as newPostAction } from "./routes/NewPost.jsx";
+import RootLayout from "./routes/RootLayout.jsx";
+import PostDetails, {loader as postDetailsLoader} from "./routes/PostDetails.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: '/create-post', element: <NewPost />, action: newPostAction },
+          { path: '/:id', element:<PostDetails/>,loader:postDetailsLoader},
+        ],
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
